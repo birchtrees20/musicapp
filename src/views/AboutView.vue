760b3803c6e633ec68156b62eccfc81612882365
birchtrees20 <150322141+birchtrees20@users.noreply.github.com>
2023-12-09 13:59:04 +0700

    Formatting

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <h1>About</h1>
+  </main>
 </template>
+
+<script>
+export default {
+
+}
+</script>
 
 <style>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
-<script setup>
-</script>
-
 <template>
   <main>
     <h1>Home</h1>
   </main>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
