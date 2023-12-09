3f6e9ea3076ac1d3fbb43e6e73ae4624ec53791b
birchtrees20 <150322141+birchtrees20@users.noreply.github.com>
2023-12-10 04:49:58 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const router = createRouter({
       }
     },
     {
-      path: '/instruments/:instrument',
+      path: '/instruments/',
       name: 'instrument',
       component: InstrumentView,
       props: true,
