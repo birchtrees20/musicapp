a6ec845f906cc14ea058eb41edba752eedee09fc
birchtrees20 <150322141+birchtrees20@users.noreply.github.com>
2023-12-10 01:51:55 +0700

    new user collection generated upon registration of user
    basically, to display all members, I wanted to retrieve all the registered users from the firebase authentication system. It turns out that to do this, you need a feature called firebase cloud functions which locked behind a paywall. To avoid this, I just made a new collection for users that manually creates users whenever one is registerd. This way, the fields can be accessed without having to use cloud functions or running a seperate server to use admin sdk from firebase
    
    tldr - added alternative to retrieve user data without using paid features from firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,72 +1,90 @@
 import { createStore } from 'vuex';
-import router from '../router'
-import { auth } from '../firebase'
-import { 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
-    signOut } from 'firebase/auth'
-
+import router from '../router';
+import { auth, db } from '../firebase'; // Import auth and db from your firebase module
+import { doc, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 export default createStore({
-    state: {
-        user: null,
-        userID: ''
+  state: {
+    user: null
+  },
+  mutations: {
+    SET_USER(state, user) {
+      state.user = user;
     },
-    mutations: {
-        SET_USER (state, user) {
-            state.user = user
-        },
-        CLEAR_USER (state) {
-            state.user = null
+    CLEAR_USER(state) {
+      state.user = null;
+    }
+  },
+  actions: {
+    async login({ commit }, details) {
+        const { email, password } = details;
+      
+        try {
+          // Sign in user using Firebase Authentication
+          await signInWithEmailAndPassword(auth, email, password);
+      
+          // Get the current user
+          const currentUser = auth.currentUser;
+      
+          if (currentUser) {
+            // Dispatch a mutation or commit to update the state or handle other logic
+            commit('SET_USER', { userID: currentUser.uid, email: currentUser.email });
+      
+            // Additional logic or actions can be added here
+      
+            // Redirect to home page after successful login
+            router.push('/');
+          }
+        } catch (error) {
+          // Handle login errors
+          switch (error.code) {
+            case 'auth/user-not-found':
+              alert("User not found. Please check your email or register for a new account.");
+              break;
+            case 'auth/wrong-password':
+              alert("Incorrect password. Please check your password and try again.");
+              break;
+            case 'auth/invalid-email':
+              alert("Invalid email address. Please provide a valid email.");
+              break;
+            default:
+              alert("An error occurred during login. Please try again later.");
+          }
+      
+          console.error('Login error:', error.message);
+          throw error; // Rethrow the error to be caught by the calling code
         }
+      },
+      
+    async register({ commit }, details) {
+      const { email, password } = details;
+
+      try {
+        // 1. Create user in Firebase Authentication
+        const { user } = await createUserWithEmailAndPassword(auth, email, password);
+
+        // 2. Create a user document in Firestore
+        const userDocRef = doc(db, 'users', user.uid);
+        await setDoc(userDocRef, {
+          userID: user.uid,
+          email: email,
+          // You can add other fields as needed
+        });
+
+        // 3. Dispatch a mutation or commit to update the state or handle other logic
+        commit('SET_USER', { userID: user.uid, email });
+
+        // Additional logic or actions can be added here
+
+        // 4. Return the user data or any other information you may need
+        return user;
+      } catch (error) {
+        // Handle registration errors
+        console.error('Registration error:', error.message);
+        throw error; // Rethrow the error to be caught by the calling code
+      }
     },
-    actions: {
-        
-        async login ({commit}, details) {
-            const { email, password} = details
-
-            try {
-                await signInWithEmailAndPassword(auth, email, password)
-            } catch (error) {
-                switch(error.code) {
-                    case 'auth/user-not-found':
-                        alert("User not found")
-                        break
-                    case 'auth/wrong-password':
-                        alert("wrong password")
-                        break
-                    default:
-                        alert(error.message)
-                }
-                return
-            }
-            commit('SET_USER', auth.currentUser)
-            router.push('/')
-        },
-        async register ({commit}, details) {
-            const { email, password} = details
-
-            try {
-                await createUserWithEmailAndPassword(auth, email, password)
-            } catch (error) {
-                switch(error.code) {
-                    case 'auth/email-already-in-use':
-                        alert("email already used")
-                        break
-                    case 'auth/invalid-email':
-                        alert("invalid email")
-                        break
-                    case 'auth/weak-password':
-                        alert("weak password")
-                        break    
-                    default:
-                        alert(error.message)
-                }
-                return
-            }
-            commit('SET_USER', auth.currentUser)
-            router.push('/')
-        },
         async logout ({commit}) {
             await signOut(auth)
 
