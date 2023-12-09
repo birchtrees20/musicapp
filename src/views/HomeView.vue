1f24315a6984eeff0fe572998246932a3ede3b8b
birchtrees20 <150322141+birchtrees20@users.noreply.github.com>
2023-12-09 18:05:01 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,35 @@
 <template>
-  <main>
+  <div>
     <h1>Home</h1>
-  </main>
+    <p>Welcome to the Home Page of Your Vue.js App!</p>
+
+    <div>
+      <h2>All Users</h2>
+      <ul>
+        <li v-for="user in users" :key="user.uid">{{ user.email }}</li>
+
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
 
-}
+const users = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/users');
+    console.log('API Response:', response.data);
+    users.value = response.data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+});
 </script>
 
 <style>
-
+/* You can add styles specific to the home component here */
 </style>
