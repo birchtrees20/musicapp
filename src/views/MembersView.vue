d79edff00a166923d017400c93005f82ece2fd48
birchtrees20 <150322141+birchtrees20@users.noreply.github.com>
2023-12-10 17:42:18 +0700

    Update MembersView.vue

--- src/views/MembersView.vue
+++ src/views/MembersView.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>All Members</h1>
 
-    <!-- Sorting Options -->
     <div>
       <label for="nameSort">Name:</label>
       <select id="nameSort" v-model="nameSort" @change="sortUsers">
@@ -11,21 +10,19 @@
       </select>
     </div>
 
-    <!-- Filtering Options -->
     <div>
       <label for="instrumentFilter">Instrument:</label>
       <select id="instrumentFilter" v-model="instrumentFilter" @change="filterUsers">
         <option value="">All Instruments</option>
-        <option v-for="instrument in allInstruments" :key="instrument" :value="instrument">
+        <option v-for="instrument in sortedInstruments" :key="instrument" :value="instrument">
           {{ instrument }}
         </option>
       </select>
     </div>
 
-    <!-- Member Tiles -->
     <div class="member-tiles">
       <router-link
-        v-for="user in filteredUsers"
+        v-for="user in displayedUsers"
         :key="user.userID"
         :to="{ name: 'profile', params: { userID: user.userID } }"
         class="member-tile"
@@ -43,16 +40,14 @@
 import { ref, onUnmounted } from 'vue';
 import { onSnapshot, collection } from 'firebase/firestore';
 import { db } from '@/firebase/index.js';
-import { emailToName } from '@/helpers/scripts.js';
 
 const allUsers = ref([]);
+const displayedUsers = ref([]);
 const allInstruments = ref([]);
-const filteredUsers = ref([]);
+const sortedInstruments = ref([]);
+let nameSort = ref('asc'); // Default sorting is ascending
+let instrumentFilter = ref('');
 
-const nameSort = ref('asc'); // Default sorting order for Name
-const instrumentFilter = ref('');
-
-// Fetch users and instruments
 onSnapshot(collection(db, 'users'), (querySnapshot) => {
   allUsers.value = [];
   allInstruments.value = [];
@@ -68,36 +63,41 @@ onSnapshot(collection(db, 'users'), (querySnapshot) => {
     };
     allUsers.value.push(user);
 
-    // Collect all unique instruments for filtering
+    //collect all unique instruments for filtering
     allInstruments.value = [...new Set([...allInstruments.value, ...userInstruments])];
   });
 
-  // Initial sorting and filtering
+  //sort instruments lexicographically by ascending order
+  sortedInstruments.value = allInstruments.value.sort();
+
+  //initial sorting and filtering
   sortUsers();
   filterUsers();
 });
 
 const sortUsers = () => {
-  const compareFunction = (a, b) => {
-    const nameA = a.firstName + a.lastName;
-    const nameB = b.firstName + b.lastName;
+  displayedUsers.value = [...allUsers.value];
 
-    return nameSort.value === 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
-  };
+  //sort members based on name
+  displayedUsers.value.sort((a, b) => {
+    const nameA = a.firstName + ' ' + a.lastName;
+    const nameB = b.firstName + ' ' + b.lastName;
 
-  allUsers.value.sort(compareFunction);
-
-  // Trigger filtering after sorting
-  filterUsers();
+    if (nameSort.value === 'asc') {
+      return nameA.localeCompare(nameB);
+    } else {
+      return nameB.localeCompare(nameA);
+    }
+  });
 };
 
 const filterUsers = () => {
-  if (!instrumentFilter.value) {
-    // No filter selected, display all users
-    filteredUsers.value = [...allUsers.value];
+  if (instrumentFilter.value) {
+    displayedUsers.value = allUsers.value.filter((user) =>
+      user.instruments.includes(instrumentFilter.value)
+    );
   } else {
-    // Filter users based on the selected instrument
-    filteredUsers.value = allUsers.value.filter((user) => user.instruments.includes(instrumentFilter.value));
+    displayedUsers.value = [...allUsers.value];
   }
 };
 </script>
@@ -113,6 +113,6 @@ const filterUsers = () => {
   padding: 16px;
   margin: 8px;
   width: 200px;
-  cursor: pointer; /* Add this line to show the tiles are clickable */
+  cursor: pointer;
 }
 </style>
